a33c108250d2363203aeafe81a984cb52362f0b8
khw970421 <khw970421@kakao.com>
2021-11-26 00:42:47 +0900

    ✨ feat : craco 동작 코드 추가

--- cra/craco.config.js
+++ cra/craco.config.js
@@ -3,13 +3,10 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@contexts": path.resolve(__dirname, "src/contexts"),
-      "@pages": path.resolve(__dirname, "src/pages"),
       "@api": path.resolve(__dirname, "src/api"),
-      "@AntDesign": path.resolve(__dirname, "src/components/AntDesign"),
-      "@SessionStorage": path.resolve(__dirname, "src/SessionStorage"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
 };
